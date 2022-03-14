e1dd1242ddad77515f0bd1b113e6014a45819b8c
Roushan <roushan@iitk.ac.in>
2022-03-14 15:56:02 +0530

    updates

--- Angular12JwtAuth/src/app/app.component.ts
+++ Angular12JwtAuth/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TokenStorageService } from './_services/token-storage.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   private roles: string[] = [];
